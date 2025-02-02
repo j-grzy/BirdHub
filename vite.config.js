52c9e8878f28689d65c208c15bdc7c4a61595f5e
j-grzy <jana_grzywacz@web.de>
2025-02-02 23:17:32 +0100

    adjust settings for docker and production

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
+  base: process.env.VITE_BASE_URL || "/",
   plugins: [react()],
   define: {
     "process.env": process.env,
   },
-});
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+}));
